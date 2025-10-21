c90a1c654726533632f5d470ea60572829a97a69
mukeshkanna11 <agriculturistmukesh11@gmail.com>
2025-10-21 17:33:54 +0530

    Commit

--- Frontend/src/components/Newsletter.jsx
+++ Frontend/src/components/Newsletter.jsx
@@ -18,7 +18,7 @@ export default function NewsletterCTA() {
     setResponseMsg(null);
 
     try {
-      const res = await axios.post("http://localhost:5000/api/contact/subscribe", { email });
+      const res = await axios.post("https://readytech-websites.onrender.com/api/contact/subscribe", { email });
       setResponseMsg({ type: "success", text: res.data.msg });
       setEmail("");
     } catch (err) {
