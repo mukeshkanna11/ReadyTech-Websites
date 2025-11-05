e9038735dcb4124d4a49a70cac226e8288f7468a
mukeshkanna11 <agriculturistmukesh11@gmail.com>
2025-11-05 12:10:02 +0530

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,6 @@ import contactRoutes from "./routes/contact.js";
 import employeeRoutes from "./routes/employees.js";
 import taskRoutes from "./routes/tasks.js";
 import attendanceRoutes from "./routes/attendance.js";
-import workRoutes from "./routes/work.js";
 import workRoutes from "./routes/workRoutes.js";
 
 // ========================================================
